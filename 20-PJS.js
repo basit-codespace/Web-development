1916dd93d91c6de7b5e8b6e130e1e1b9b55476b4
basit-codespace <abdulbasit.dev2026@gmail.com>
2026-04-09 01:56:58 -0700

    Added NAVTTAC Code

---
+++ 20-PJS.js
@@ -0,0 +1,13 @@
+Let num1 = 10;
+Let num2 = 5;
+Let operator = “*”;
+
+Let result;
+
+If (operator === “+”) result = num1 + num2;
+Else if (operator === “-“) result = num1 – num2;
+Else if (operator === “*”) result = num1 * num2;
+Else if (operator === “/”) result = num1 / num2;
+Else result = “Invalid operator”;
+
+Console.log(“Result:”, result);
